a11afd2298857834fc4e85fac5e1d232d9d6d96d
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2019-07-24 15:49:33 -0400

    added logout routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,20 +9,24 @@ class SessionsController < ApplicationController
     def create
        #find the user
        @user = User.find_by(username: params[:user][:username])
-  
+       #did we find the user & did they put in the right password?
+       
        #if we find something & they have the right password
        if @user && @user.authenticate(params[:user][:password])
-         session[:user_id] = @user.id
-         redirect_to user_path(@user.id)
-       else
-          flash[:message] = "Sorry, please try again"
+        
+        if @user.try(:authenticate, params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          flash[:error] = "Sorry, login info was incorrect. Please try again."
+          #redirect to login to make it harder for account break-ins
           redirect_to login_path
-       end
-  
+        end
+      end
     end
   
     def destroy
-      session.clear
+      session.delete(:user_id)
       redirect_to root_path
     end
   end
