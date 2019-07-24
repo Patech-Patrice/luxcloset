b2016e867bf1d7c1b5d53ffb087f227b3484363c
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2019-07-24 13:43:35 -0400

    created template for sessions route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
-end
+    #this method instantiates an empty new user object and renders the sign_up form
+    #once a user is saved, they are redirected to the root url
+      #code...
+    #loading the signup form
+    def new
+      @user = User.new
+    end
+    
+    #signup
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        #login the user
+        session[:user_id] = @user.id #stores @user.id in a cookie session
+        redirect_to user_path(@user)
+        #flash message to welcome new users
+        #notice = "Welcome to your Luxury Closet Organizer!"
+      else
+        render :new
+      end
+    end
+    
+    def show
+      @user = User.find_by_id(params[:id])
+    redirect_to '/' if !@user
+    end
+    
+    private
+    
+    def user_params
+      params.require(:user).permit( :username, :email, :password)
+    end
+    
+    
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  #root 'sessions#home'
+  root 'sessions#home'
   get '/signup' => 'users#new'
- # get '/login' => 'sessions#new'
+  get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  #delete '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
