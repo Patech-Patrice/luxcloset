f3d78746d74463d7b89617e024dc98e4ace40d8d
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2019-07-24 16:37:00 -0400

    added index route for shoes and handbags

--- app/controllers/handbags_controller.rb
+++ app/controllers/handbags_controller.rb
@@ -1,2 +1,65 @@
 class HandbagsController < ApplicationController
-end
+    
+        def index
+            @handbags = Handbag.all
+           end
+         
+           def show
+           @handbag = Handbag.find(params[:id])
+           end
+         
+         
+           def new
+             @handbag = Handbag.new
+           end
+         
+         
+           def create
+             #@shoe = current_user.shoes.build(shoe_params)
+             @handbag = Handbag.create(handbag_params)
+             @handbag.user_id = session[:user_id]
+             #byebug
+             if @handbag.save
+               redirect_to handbag_path(@handbag)
+             else
+               render :new
+         
+           end
+         end
+         
+         private
+         
+           def show
+             #set_shoe
+             @handbag = Handbag.find( params[:id])
+           end
+         
+         
+         
+           #def destroy
+             #set_shoe
+             #@shoe.destroy
+             #redirect_to shoes_path
+           #end
+         
+         
+         
+         
+           # this method calls the Shoe.find method to retrieve the shoe with an params corresponding to the params passed to the controller.
+           #def set_shoe
+              #@shoe = Shoe.find_by(id: params[:id])
+              #@shoe = Shoe.find(params[:user_id], params[:name], params[:brand], params[:url], params[:color], params[:designer_id], params[:style])
+             #if !@shoe
+               #redirect_to shoes_path
+           #end
+         #end
+         
+           #strong params
+           def handbag_params
+               params.require(:shoe).permit(:name, designer_attributes:[:name])
+           end
+         
+         end
+    
+    
+
