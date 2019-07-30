cdd47062169df3febeb50a9fac9762e35840a266
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2019-07-30 13:07:08 -0400

    added reviews to create join table

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+
+    def def new
+        @review = Review.new
+    end
+
+    def create
+    end
+    
+    def show
+    end
+
+
+    def index
+    end
+
+    private
+
+    def review_params
+    end
+
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "designers", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 4) do
     t.datetime "updated_at", null: false
     t.index ["designer_id"], name: "index_handbags_on_designer_id"
     t.index ["user_id"], name: "index_handbags_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.string "title"
+    t.string "content"
+    t.integer "user_id"
+    t.integer "designer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["designer_id"], name: "index_reviews_on_designer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shoes", force: :cascade do |t|
